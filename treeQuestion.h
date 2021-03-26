b78d0f6ed1f281743d077256ccd46b6a841b0073
xzz-123 <1805402615@qq.com>
2021-03-26 11:09:24 +0800

    添加了二叉树序列化和反序列化问题

--- treeQuestion.h
+++ treeQuestion.h
@@ -5,6 +5,7 @@
 #include <stack>
 #include <unordered_set>
 #include <map>
+#include <sstream>
 #include "listQuestion.h"
 using namespace std;
 struct TreeNode {
@@ -208,4 +209,45 @@ public:
 	int sum(string prefix);
 private:
 	map<string, int>mp;
+};
+//297. Serialize and Deserialize Binary Tree
+class Codec {
+public:
+
+	// Encodes a tree to a single string.
+	string serialize(TreeNode* root) {
+		ostringstream out;
+		serialize(root, out);
+		return out.str();
+	}
+
+	// Decodes your encoded data to tree.
+	TreeNode* deserialize(string data) {
+		istringstream in(data);
+		return deserialize(in);
+	}
+
+private:
+	void serialize(TreeNode* root, ostringstream& out) {
+		if (root) {
+			out << root->val << ' ';
+			serialize(root->left, out);
+			serialize(root->right, out);
+		}
+		else {
+			out << "# ";
+		}
+	}
+
+	TreeNode* deserialize(istringstream& in) {
+		string val;
+		in >> val;
+		if (val != "#") {
+			TreeNode* root = new TreeNode(stoi(val));
+			root->left = deserialize(in);
+			root->right = deserialize(in);
+			return root;
+		}
+		else return nullptr;
+	}
 };
