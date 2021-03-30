4a0d76c0c2cdf3a12cea99807a3d5800f72e9305
xzz <1805402615@qq.com>
2021-03-30 22:34:27 +0800

    cutrope problem

---
+++ dynamicPlannging.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include<algorithm>
+using namespace std;
+//剪绳子
+//给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 
+//可能的最大乘积是多少？
+class CutRope {
+public:
+	int cuttingRope(int n) {
+		vector<int>dp(n + 1);
+		dp[2] = 1;
+		for (int i = 3;i < n + 1;++i) {
+			for (int j = 2;j < i;++j) {
+				dp[i] = max(dp[i], max(j*(i - j), j*dp[i - j]));
+			}
+		}
+		return dp[n];
+	}
+};
